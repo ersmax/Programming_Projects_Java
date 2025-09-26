bf8bb68cef1e1fc13bb0f9aca3a178ead53d42dd
ersmax <e.masarin@student.vu.nl>
2025-09-26 16:06:51 +0200

    Add documentation

--- src/Projects_set1/Project_01_BMI/BMI.java
+++ src/Projects_set1/Project_01_BMI/BMI.java
@@ -1,17 +1,15 @@
-/*
-Body Mass Index (BMI) helps in specifying the weight category a person
-belongs to, depending on their body weight. BMI is estimated using the following
-formula:
-BMI = (Weight in kilograms)/(Height in meters)^2
-Write a program that calculates and outputs the BMI. Assume various input values
-wherever required.
- */
-
 package Projects_set1.Project_01_BMI;
 
 import java.util.Scanner;
 
 public class BMI {
+
+    /**
+     * Precondition: None
+     * Postcondition: This functions show BMI starting
+     * from weigth and height
+     * @param args
+     */
     public static void main(String[] args) {
         float weight, height, BMI;
         Scanner input = new Scanner(System.in);
--- src/Projects_set1/Project_02_Arcade/Arcade.java
+++ src/Projects_set1/Project_02_Arcade/Arcade.java
@@ -1,22 +1,15 @@
-/*
-The video game machines at your local arcade output coupons according to
-how well you play the game. You can redeem 10 coupons for a candy bar or 3
-coupons for a gumball. You prefer candy bars to gumballs. Write a program that
-defines a variable initially assigned to the number of coupons you win. Next,
-the program should output how many candy bars and gumballs you can get if
-you spend all of your coupons on candy bars first, and any remaining coupons
-on gumballs.
- */
-
-
 package Projects_set1.Project_02_Arcade;
-
-
-
 
 public class Arcade {
     public static final int CANDY_BAR_PRICE = 10, GUMBALL_PRICE = 3;
 
+    /**
+     * Main method
+     * Precondtions: None
+     * Postconditions: Prints the number of candy bars and gumballs that can be
+     * obtained with a given number of coupons, prioritizing candy bars first.
+     * @param args
+     */
     public static void main(String[] args) {
         int numberCoupons = 105, candyBars, gumBalls;
         int couponsRemaining;
